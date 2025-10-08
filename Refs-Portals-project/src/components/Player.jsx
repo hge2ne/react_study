438a5e5fd9f7cf49629c4ef6cdef37e6247cc787
hge2ne <hge2ne@gmail.com>
2025-10-08 15:28:35 +0900

    Section8 first commit

---
+++ Refs-Portals-project/src/components/Player.jsx
@@ -0,0 +1,12 @@
+export default function Player() {
+  // 과제 : 컴포넌트 작동시켜서 이름 설정하도록 하기
+  return (
+    <section id="player">
+      <h2>Welcome unknown entity</h2>
+      <p>
+        <input type="text" />
+        <button>Set Name</button>
+      </p>
+    </section>
+  );
+}
