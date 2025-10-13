41e0eb58f7909430679db92a3d5886e958a53001
hge2ne <hge2ne@gmail.com>
2025-10-14 00:12:31 +0900

    tailwindcss로 버튼과 입력 항목 스타일링

--- project_management/src/components/NewProject.jsx
+++ project_management/src/components/NewProject.jsx
@@ -2,19 +2,20 @@ import Input from "./input";
 
 export default function NewProject() {
   return (
-    <div>
-      <menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button>Cancel</button>
+          <button className="text-800 hover:text-stone-950">Cancel</button>
         </li>
         <li>
-          <button>Save</button>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone 950">
+            Save
+          </button>
         </li>
       </menu>
       <div>
         <Input label="Title" />
-        <Input label="Description" textarea={True} />{" "}
-        {/* textarea={True} 대신 textarea 해도 True로 설정됨(반대 !) */}
+        <Input label="Description" textarea={true} />
         <Input label="Due Date" />
       </div>
     </div>
