2f323e3e81bc518f56211fa7e2c73cac09ca8a2a
hge2ne <hge2ne@gmail.com>
2025-10-17 14:09:13 +0900

    Side Effect(부수 효과)가 무엇인지 예시를 통한 소개

--- useeffect-hook/src/App.jsx
+++ useeffect-hook/src/App.jsx
@@ -1,16 +1,33 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
+  navigator.geolocation.getCurrentPosition((position) => {
+    const sortedPlaces = sortPlacesByDistance(
+      AVAILABLE_PLACES,
+      position.coords.latitude, //위도
+      position.coords.longitude //경도
+    );
+  });
+
+  /* 
+  - navigator : 브라우저가 해당 브라우저 내에서 구동되는 js 코드에 노출시키는 객체임(브라우저 제공 객체)
+  - 메서드 호출 시 사용자는 위치 제공 여부 요청 받음, 동의 후 위치파악 진행됨
+  - 위치파악은 시간이 걸리는 작업이므로, 파라미터로 콜백함수 입력받음
+  - (주의) 위 함수는 브라우저로부터 호출받음. 사용자 위치가 포함된 객체를 제공하는 것도 브라우저임.
+  - navigator 부터 전체 코드 : side effect임
+  - 이유: 이 코드와 사용자 위치가 이 앱에 필요하지만 App 컴포넌트의 핵심 기능 x, 즉각 실행 x, 직접적 영향 주지않기 때문
+*/
   function handleStartRemovePlace(id) {
     modal.current.open();
     selectedPlace.current = id;
@@ -57,7 +74,7 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
+          fallbackText={"Select the places you would like to visit below."}
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
