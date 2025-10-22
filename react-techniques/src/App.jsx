9549831cda5fb6e234969b28a8be48b7f49c46f8
hge2ne <hge2ne@gmail.com>
2025-10-22 14:03:20 +0900

    214. State(상태) 스케줄링 & 배칭

--- react-techniques/src/App.jsx
+++ react-techniques/src/App.jsx
@@ -12,6 +12,16 @@ function App() {
 
   function handleSetCount(newCount) {
     setChosenCount(newCount);
+    //setChosenCount(chosenCount + 1) : useState 초기값 0 받아와서 시작(출력 1)
+    setChosenCount((prevChosenCount) => prevChosenCount + 1);
+    /* 
+    위 방법 : state 업데이트 함수웨 함수 전달
+    => 그 함수가 이전 state 스냅샷 전달 받고 그 새로운 state 스냅샷 반환
+    (리액트가 가장 최신 state 스냅샷 보장. 이때 받는 newCount 값은 setChosenCount(newCount) stata업데이트에서 설정된 값 )
+    - 여러 state 업데이트 실행되는 경우, 순서대로 실행됨
+    - 배칭 이란? :
+    같은 함수 내에서 여러 state 업데이트가 있을 때 다같이 배칭되어서 컴포넌트 1회만 실행하는 것
+     */
   }
   // 새로운 카운트를 입력값과 매개변수로 받는 용도
   return (
