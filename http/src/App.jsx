a8a42dc930f89896c835f0540a7ed99a473cda86
hge2ne <hge2ne@gmail.com>
2025-10-27 18:20:24 +0900

    POST 요청으로 데이터 전송

--- http/src/App.jsx
+++ http/src/App.jsx
@@ -1,16 +1,17 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback } from "react";
 
-import Places from './components/Places.jsx';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-import AvailablePlaces from './components/AvailablePlaces.jsx';
+import Places from "./components/Places.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
+import { updateUserPlaces } from "./http.js";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
-
+  //userPlaces : 장소를 선택할때마다 업데이트되는 장소 배열
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   function handleStartRemovePlace(place) {
@@ -21,8 +22,11 @@ function App() {
   function handleStopRemovePlace() {
     setModalIsOpen(false);
   }
-
-  function handleSelectPlace(selectedPlace) {
+  /**
+   *
+   * @handleSelectPlace : async/ await 추가함으로써 이벤트리스너 함수로 만들어줌(유저가 장소선택할때 실행됨)
+   */
+  async function handleSelectPlace(selectedPlace) {
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,6 +36,10 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    try {
+      await updateUserPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {}
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
--- http/src/http.js
+++ http/src/http.js
@@ -9,9 +9,26 @@ try : 실패할 수 있는 코드를 {} 로 감쌈
 catch : error를 props 로 넣고 에러 발생했을 때 실행할 코드는 {}에 정의
 */
   if (!response.ok) {
-    throw new ErrorPage("Failed to fetch places");
+    throw new Error("Failed to fetch places");
   }
   return resData.places;
+}
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT", //method 라는 속성을 정의
+    body: JSON.stringify({ places: places }), // places 배열을 JSON 형식으로 변환
+    //(주의) 백엔드는 places key를 가진 객체만 요청받고 응답해줌 {}로 감싸주고 key 추가
+    // keydhk 값을 가진 변수가 같은 이름이므로 {places} 로 생략 가능
+    headers: {
+      "Content-Type": "application/json",
+    }, //요청에 첨부될 추가 메타데이터
+  });
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user data.");
+  }
+  return resData.message; //message 속성 붙는 이유? 백엔드 API 보면 message 속성을 응답으로 받고 있기 때문
 }
 
 /**
