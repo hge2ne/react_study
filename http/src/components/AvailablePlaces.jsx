7b5ffe41f462b759cb160fc2d23ae732257ab5c9
hge2ne <hge2ne@gmail.com>
2025-10-27 12:41:08 +0900

    로딩 state 다루기

--- http/src/components/AvailablePlaces.jsx
+++ http/src/components/AvailablePlaces.jsx
@@ -4,13 +4,15 @@ import {useState, useEffect} from 'react'
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([])
+  const [isFetching, setIsFetching] = useState(false) //초기값 false 설정. useEffect 함수 내에선 True 로 설정
 
   useEffect(()=> {
+    setIsFetching(true) // true인 이유? places를 가져오기 시작하기 때문
     async function fetchPlaces(){
       //async 추가(개발자가 정의한 함수이기 때문에 리액트가 사용하려면 추가해야함)
       const response = await fetch('http://localhost:3000/places')
       const resData = await response.json()
-      setAvailablePlaces(resData.places)
+      setAvailablePlaces(false)
     }
 
     fetchPlaces() //함수 생성하고 반드시 호출해야 작동함
@@ -25,7 +27,9 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces} //위에서 useEffect 작업 후 여기서 사용 가능
-      fallbackText="No places available."
+      isLoading={true} //places 를 기다리는 동안 true 전달 데이터 받으면 false로 전환 
+      loadingText="Fetching place data..." //대체 텍스트
+      fallbackText="No places available." //저장된 장소가 없을 때 띄우는 대체 텍스트(fallback)
       onSelectPlace={onSelectPlace}
     />
   );
